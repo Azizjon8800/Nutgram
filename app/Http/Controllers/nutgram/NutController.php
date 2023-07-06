3126735c7b49b0f49f1c7d3b226ce6b5f912f2d4
Azizjon8800 <azizjon8437@gmail.com>
2023-07-06 15:42:59 +0500

    Extend

--- app/Http/Controllers/nutgram/NutController.php
+++ app/Http/Controllers/nutgram/NutController.php
@@ -15,15 +15,16 @@ class NutController extends Controller
         $bot = new Nutgram($_ENV['TELEGRAM_TOKEN']);
         $bot ->setRunningMode(Polling::class);
 
-        $bot->onCommand('start', function(Nutgram $bot) {
-            $bot->sendMessage('Ciao!');
+        Nutgram::macro('sendHelloMessage', function() {
+            return $this->sendMessage('Hello message!', ['chat_id' => 1153216 ]);
         });
 
-        $bot->onText('My name is {name}', function(Nutgram $bot, string $name) {
-            $bot->sendMessage("Hi $name");
+        Message::macro('pin', function(array $opt = []) {
+            return $this->pinChatMessage($this->chat->id, $this->message_id, $opt);
         });
 
-
+        $message = $bot->sendHelloMessage();
+        $message->pin();
         $bot->run();
     }
 }
